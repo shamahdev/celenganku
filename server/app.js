986919fc22888ee8fedb08b63eeef441b22248f5
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2021-02-09 01:35:33 +0700

    Fix Webpack Middleware break production build

--- server/app.js
+++ server/app.js
@@ -1,7 +1,3 @@
-import webpack from 'webpack'
-import webpackDevMiddleware from 'webpack-dev-middleware'
-import webpackHotMiddleware from 'webpack-hot-middleware'
-
 import 'regenerator-runtime'
 import express from 'express'
 import path from 'path'
@@ -11,8 +7,6 @@ import helmet from 'helmet'
 import hpp from 'hpp'
 import xss from 'xss-clean'
 import cookirParser from 'cookie-parser'
-import CONFIG from './global/config'
-import config from '../webpack.dev'
 
 import siswaRoutes from './routes/siswa-routes'
 import adminRoutes from './routes/admin-routes'
@@ -20,12 +14,6 @@ import AuthController from './controllers/auth-controller'
 
 const app = express()
 const HTML_FILE = path.join(__dirname, 'index.html')
-
-const compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-}))
-app.use(webpackHotMiddleware(compiler))
 
 app.use(cors())
 app.use(helmet())
@@ -45,16 +33,5 @@ app.use('/logout', AuthController.logout)
 app.use(bodyParser.json())
 app.use(express.static(__dirname))
 app.use('/', (req, res) => res.sendFile(HTML_FILE))
-
-app.get('*', (req, res, next) => {
-  compiler.outputFileSystem.readFile(path.join(CONFIG.DIST_DIR, 'index.html'), (err, result) => {
-    if (err) {
-      return next(err)
-    }
-    res.set('content-type', 'text/html')
-    res.send(result)
-    res.end()
-  })
-})
 
 export default app
