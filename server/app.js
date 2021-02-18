5dba5c656dbec8da4fbcf57091432392a706336d
Shaddam Amru Hasibuan <shaddam.a.h@gmail.com>
2021-02-19 04:44:38 +0700

    Fix production server

--- server/app.js
+++ server/app.js
@@ -4,21 +4,16 @@ import path from 'path'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
-import multer from 'multer'
+import appRoutes from './routes/app-routes'
 import siswaRoutes from './routes/siswa-routes'
 import adminRoutes from './routes/admin-routes'
-import AuthController from './controllers/auth-controller'
+import transactionRoutes from './routes/transaction-routes'
 
 const app = express()
 const HTML_FILE = path.join(__dirname, 'index.html')
 
-const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: {
-    fileSize: 5 * 1024 * 1024, // no larger than 5mb, you can change as needed.
-  },
-})
 app.use(cors())
+app.options('*', cors())
 app.use(cookieParser())
 app.use(express.json({
   limit: '15kb',
@@ -27,12 +22,10 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }))
 
+app.use('/api', appRoutes)
 app.use('/api/siswa', siswaRoutes)
 app.use('/api/admin', adminRoutes)
-app.get('/token', AuthController.requireAuth, AuthController.retrieveToken)
-app.post('/upload', upload.single('avatar'), AuthController.uploadFile)
-app.post('/delete', AuthController.deleteFile)
-app.use('/logout', AuthController.logout)
+app.use('/api/transaction', transactionRoutes)
 app.use(express.static(__dirname))
 app.use('/', (req, res) => res.sendFile(HTML_FILE))
 
